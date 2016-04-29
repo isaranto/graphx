ec187e702d3ce48fe06fed11b2801f241f2916d8
isaranto <isarand@gmail.com>
2016-04-29 15:51:02 +0300

    [DEV] Implemented degree centrality(complete)

--- src/main/java/org/centrality/spark/MyGraph.scala
+++ src/main/java/org/centrality/spark/MyGraph.scala
@@ -1,5 +1,6 @@
 package org.centrality.spark
 import scala.io.Source
+import scala.collection.mutable.HashMap
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.graphx._
 
@@ -16,19 +17,25 @@ object MyGraph {
    }
 
   def main(args: Array[String]) {
-    //val distFile = "/home/lias/twitter.edges"
     System.setProperty("hadoop.home.dir", "/home/lias/IdeaProjects/centrality/")
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
     val sc = new SparkContext(conf)
-
-    //val newRDD = sc.textFile("twitter_edges");
     //val distFile = sc.textFile(edgesFile())
-    val graph = GraphLoader.edgeListFile(sc, "twitter_edges")
-    var degrees:Map[Long,Int] = Map()
+    //val graph = GraphLoader.edgeListFile(sc, "twitter_edges.txt")
+    val graph = GraphLoader.edgeListFile(sc, "followers.txt")
+    /*var degrees:Map[Long,Int] = Map()
     for (triplet <- graph.triplets.collect) {
-        println(s"${triplet.srcId} likes ${triplet.dstId}")}
-
-
+        println(s"${triplet.srcId} follows ${triplet.dstId}")}*/
+    //var degrees: HashMap[Long, Int] = HashMap()
+    var test = graph.vertices.collect().toMap
+    val degrees: VertexRDD[Int] = graph.degrees
+    println(s"The graph has a total of $degrees.count() vertices")
+    //sort vertices on descending degree value
+    val sorted = degrees.sortBy(- _._2)
+    //print top 10 vertices
+    for ((vertexId, degree) <- sorted.take(10)){
+      println(s"Vertex with id ${vertexId} has a degree of ${degree}")
+    }
   }
 
 
