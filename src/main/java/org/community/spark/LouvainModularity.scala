a61a69899530fe3d46160a500ccf536c58bd84eb
isaranto <isarand@gmail.com>
2016-06-24 18:42:16 +0300

    [DEV] modularity function and Phase 1 ready

--- src/main/java/org/community/spark/LouvainModularity.scala
+++ src/main/java/org/community/spark/LouvainModularity.scala
@@ -7,7 +7,7 @@ import org.apache.spark.{SparkConf, SparkContext, rdd}
 /**
   * Created by lias on 6/21/16.
   */
-object LouvainModularity {
+object LouvainModularity{
   def main(args: Array[String]) {
     val time0 = System.currentTimeMillis()
     System.setProperty("hadoop.home.dir", "/home/lias/IdeaProjects/centrality/")
@@ -22,29 +22,42 @@ object LouvainModularity {
       s"has " +
       s"neighbours ${x._2.deep.toString()}"))
     //initial.vertices.collect.foreach(println)
+    // m is the m from the dQ function of modularity(total number of edges)
     var iter = 4
     do {
+      var m = graph.edges.collect.length/2
+      var degrees = graph.degrees
       for (vertex <- initial.vertices.collect()) {
-        //get the array of the neighbours for the specific vertex
-        var dQs: rdd.RDD[(Long, Long)] = sc.emptyRDD
+        //get the array of the neighbours for the specific vertex(and append the id of itself)
+        var dQs: rdd.RDD[(Long, Double)] = sc.emptyRDD
         //TODO in/out edges or Either???
-        var neighbours = graph.collectNeighborIds(EdgeDirection.Out).filter(x => x._1 == vertex._1).map(_._2).first()
-        /*neighbours.collect.foreach(x => println(s"vertex with id ${x._1} " +
-          s"has " +
-          s"neighbours ${x._2.deep.toString()}"))*/
-        //TODO calculate modularity for the vertexid itself
+        var neighbours = graph.collectNeighborIds(EdgeDirection.Either).filter(x => x._1 == vertex._1).map(_._2)
+          .first() ++ Array(vertex._1)
+        //println(s"vertex with id ${vertex._1} has neighbours ${neighbours.deep.toString()}")
+        var k_i = graph.degrees.filter(x => x._1 == vertex._1)
         for (neighbour <- neighbours) {
           //println(s"vertex with id ${vertex._1} has neighbour ${neighbour}")
           //calculate modularity if assignment is changed
-          var dq = Array((neighbour, 2.toLong)) //modularity calculation for assignment to neighbor
+          //var dq = Array((neighbour, 2.toLong)) //modularity calculation for assignment to neighbor
+          //get graph.Vertex from id
+          var n = graph.vertices.filter(x => x._1 == neighbour).first()
+          var k_i = graph.degrees.collect.filter(x => x._1 == vertex._1).map(x => x._2).toSeq(0)
+          var k_i_inC = graph.edges.filter(e => (e.dstId==vertex._1 || e.srcId ==vertex._1)&& (e.dstId==neighbour ||
+            e.srcId ==neighbour)).count()
+          var Sin = graph.edges.filter(e => (e.dstId==neighbour && e.srcId==neighbour)||(e.srcId==neighbour && e
+            .dstId==neighbour)).count()
+          var Stot = graph.edges.filter(e => e.dstId==neighbour).count()
+          var dq = Array((neighbour,(((Sin+k_i_inC)/2*m)- math.pow((Stot+k_i)/2*m,2))-(Sin/2*m -math.pow(Stot/2*m,2) -
+            math.pow(k_i/2*m,2))))
           //create RDD with the new dq
           var newRDD = sc.parallelize(dq)
           //union with the initial dQ RDD
           dQs = dQs.union(newRDD)
         }
         dQs.collect.foreach(x => println(s" vid: ${x._1} has value ${x._2}"))
-        var assignment = dQs.collect.maxBy(_._2)._1.toInt //TODO try and catch exception java.lang
-        // .UnsupportedOperationException: empty.maxBy
+        //check if there exists a positive value. If not vertex will not change community
+        var maxQ = dQs.collect.maxBy(_._2)._2
+        var assignment  = if (maxQ > 0) dQs.collect.maxBy(_._2)._1.toInt else vertex._1.toInt
         println(s"max is ${assignment}")
         graph = graph.mapVertices((id, attr) => if (id == vertex._1) assignment else attr)
       }
